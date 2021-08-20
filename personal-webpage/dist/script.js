1ab6b9ff502e044903d7f5708154f7c0cabf3e18
SunjungAn <55094745+sunjungAn@users.noreply.github.com>
2021-08-20 15:51:51 +0900

    Add files via upload

--- personal-webpage/dist/script.js
+++ personal-webpage/dist/script.js
@@ -76,131 +76,13 @@ $(document).ready(function () {
 
   // initiate full page scroll
 
+
   $("#fullpage").fullpage({
     scrollBar: true,
     responsiveWidth: 400,
-    navigation: true,
-    navigationTooltips: ["home", "about", "portfolio", "contact", "connect"],
-    anchors: ["home", "about", "portfolio", "contact", "connect"],
     menu: "#myMenu",
-    fitToSection: false,
-
-    afterLoad: function (anchorLink, index) {
-      var loadedSection = $(this);
-
-      //using index
-      if (index == 1) {
-        /* add opacity to arrow */
-        $(".fa-chevron-down").each(function () {
-          $(this).css("opacity", "1");
-        });
-        $(".header-links a").each(function () {
-          $(this).css("color", "white");
-        });
-        $(".header-links").css("background-color", "transparent");
-      } else if (index != 1) {
-        $(".header-links a").each(function () {
-          $(this).css("color", "black");
-        });
-        $(".header-links").css("background-color", "white");
-      }
-
-      //using index
-      if (index == 2) {
-        /* animate skill bars */
-        $(".skillbar").each(function () {
-          $(this)
-            .find(".skillbar-bar")
-            .animate(
-              {
-                width: $(this).attr("data-percent")
-              },
-              2500
-            );
-        });
-      }
-    }
+    fitToSection: false
   });
-
-  // move section down one
-  $(document).on("click", "#moveDown", function () {
-    $.fn.fullpage.moveSectionDown();
-  });
-
-  // fullpage.js link navigation
-  $(document).on("click", "#skills", function () {
-    $.fn.fullpage.moveTo(2);
-  });
-
-  $(document).on("click", "#projects", function () {
-    $.fn.fullpage.moveTo(3);
-  });
-
-  $(document).on("click", "#contact", function () {
-    $.fn.fullpage.moveTo(4);
-  });
-
-
-
-  // smooth scrolling
-  $(function () {
-    $("a[href*=#]:not([href=#])").click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html,body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            700
-          );
-          return false;
-        }
-      }
-    });
-  });
-
-  // This is "probably" IE9 compatible but will need some fallbacks for IE8
-// - (event listeners, forEach loop)
-
-// wait for the entire page to finish loading
-window.addEventListener('load', function() {
-  
-  // setTimeout to simulate the delay from a real page load
-  setTimeout(lazyLoad, 1000);
-  
-});
-
-function lazyLoad() {
-  var card_images = document.querySelectorAll('.card-image');
-  
-  // loop over each card image
-  card_images.forEach(function(card_image) {
-    var image_url = card_image.getAttribute('data-image-full');
-    var content_image = card_image.querySelector('img');
-    
-    // change the src of the content image to load the new high res photo
-    content_image.src = image_url;
-    
-    // listen for load event when the new photo is finished loading
-    content_image.addEventListener('load', function() {
-      // swap out the visible background image with the new fully downloaded photo
-      card_image.style.backgroundImage = 'url(' + image_url + ')';
-      // add a class to remove the blur filter to smoothly transition the image change
-      card_image.className = card_image.className + ' is-loaded';
-    });
-    
-  });
-  
-}
-
 
 
   //ajax form
@@ -220,38 +102,6 @@ function lazyLoad() {
       var formData = $(form).serialize();
 
       // Submit the form using AJAX.
-      $.ajax({
-        type: "POST",
-        url: $(form).attr("action"),
-        data: formData
-      })
-        .done(function (response) {
-          // Make sure that the formMessages div has the 'success' class.
-          $(formMessages).removeClass("error");
-          $(formMessages).addClass("success");
-
-          // Set the message text.
-          $(formMessages).text(response);
-
-          // Clear the form.
-          $("#name").val("");
-          $("#email").val("");
-          $("#message").val("");
-        })
-        .fail(function (data) {
-          // Make sure that the formMessages div has the 'error' class.
-          $(formMessages).removeClass("success");
-          $(formMessages).addClass("error");
-
-          // Set the message text.
-          if (data.responseText !== "") {
-            $(formMessages).text(data.responseText);
-          } else {
-            $(formMessages).text(
-              "Oops! An error occured and your message could not be sent."
-            );
-          }
-        });
     });
   });
 
